0051d944e3c42032ab97ddb2af08663dc2d45f1e
lila <lila@DESKTOP-IGMFGCT>
2022-05-17 19:02:27 -0500

    Add project files.

---
+++ Practice03/Program.cs
@@ -0,0 +1,19 @@
+﻿// 3 - Develop an algorithm that prints all odd numbers from 1 to 10.
+
+using System;
+
+class Program3
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("[All odd number from 1 to 10]\n");
+
+        for (int num = 1; num < 11; num++)
+        {
+            if (num % 2 == 1) 
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
